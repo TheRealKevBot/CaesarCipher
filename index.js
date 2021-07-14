a32ace72b9690de346b8aac67f9d4714b79aa5d8
TheRealKevBot <Kglas001@gmail.com>
2021-07-14 14:02:15 -0600

    First

---
+++ index.js
@@ -0,0 +1,16 @@
+// Write a class that, when given a string, will return an uppercase string with 
+// each letter shifted forward in the alphabet by however many spots the cipher 
+// was initialized to.
+
+// For example:
+
+// var c = new CaesarCipher(5); // creates a CipherHelper with a shift of five
+// c.encode('Codewars'); // returns 'HTIJBFWX'
+// c.decode('BFKKQJX'); // returns 'WAFFLES' 
+
+var CaesarCipher = function (shift) {
+    
+  }; 
+
+  console.log(CaesarCipher('BFKKQJX'), 'WAFFLES')
+  console.log(CaesarCipher('Codewars'), 'HTIJBFWX') 
